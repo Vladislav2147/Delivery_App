168b170940480e3a8bfb8d277249c57ad1f15d6e
Vladislav2147 <vandl3511@gmail.com>
2021-04-27 19:59:41 +0300

    detailed details but without products

--- courier_application/app/src/main/java/by/bstu/vs/stpms/courier_application/ui/main/details/AvailableOrderDetailsFragment.kt
+++ courier_application/app/src/main/java/by/bstu/vs/stpms/courier_application/ui/main/details/AvailableOrderDetailsFragment.kt
@@ -1,16 +1,21 @@
 package by.bstu.vs.stpms.courier_application.ui.main.details
 
-import androidx.lifecycle.ViewModelProvider
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import by.bstu.vs.stpms.courier_application.R
 import by.bstu.vs.stpms.courier_application.databinding.FragmentAvailableOrderDetailsBinding
 import by.bstu.vs.stpms.courier_application.ui.util.OrderAdapter
+import com.google.android.material.button.MaterialButton
+
 
 class AvailableOrderDetailsFragment : Fragment() {
 
@@ -43,6 +48,33 @@ class AvailableOrderDetailsFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        (activity as AppCompatActivity?)?.supportActionBar?.let {
+            it.setDisplayShowCustomEnabled(false)
+            it.setDisplayHomeAsUpEnabled(false)
+        }
+
+        initButtons()
+    }
+
+    fun initButtons() {
+        val callButton = requireView().findViewById<MaterialButton>(R.id.btn_details_call)
+        val mapButton = requireView().findViewById<MaterialButton>(R.id.btn_details_map)
+
+        callButton.setOnClickListener {
+            val phone = viewModel.orderLiveData.value?.customer?.phone
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tel:${phone}")
+            startActivity(intent)
+        }
+
+        mapButton.setOnClickListener {
+            val address = viewModel.orderLiveData.value?.address
+            val gmmIntentUri =
+                Uri.parse("geo:0,0?q=${address}")
+            val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+            mapIntent.setPackage("com.google.android.apps.maps")
+            startActivity(mapIntent)
+        }
     }
 
 }
